599b1ed4fd950fc9caf599de75c6accd34b4a36b
abhishek271 <abhishek227@msn.com>
2024-10-04 21:45:54 +0200

    Pushed code to new repo

---
+++ README.md
@@ -0,0 +1,39 @@
+# Advesarial Neural Network Toolbox
+
+This tool was used to perform experiments for our paper "Properties that allow or prohibit transferability of adversarial examples" published on the [5th ACM/IEEE International Conference on Automation of Software Test](https://conf.researchr.org/home/ast-2024#the-5th-acmieee-international-conference-on-automation-of-software-test-ast-2024) .  
+
+All references and works used along with libraries are mentioned within this readme and also within codes. 
+
+--------------------------------------------------------
+The source code in the repository is a Python API that is able to fulfill a complete workflow of:
+1. Traning full-precision and quantized neural networks.
+2. Creating adversarial examples on these networks.
+3. Transfering adversarial examples from the network where samples are created (source) to another (target) network.
+
+The API uses Tensropack [(Y. Wu et al., 2016)](https://github.com/tensorpack) for training. Tensorpack is training framework which a part of TensorFlow 1.13 [(Abadi et al., 2016)](https://www.tensorflow.org) API.  
+For quantization DoReFa-Net method [(Zhou et al., 2018)](https://arxiv.org/abs/1606.06160) is used
+For adversarial attack generation ART [(Nicolae et al., 2019)](https://arxiv.org/abs/1807.01069 ) is used. 
+
+The API is fairly simple to use. Details on how to use the api is in the [wiki](https://github.com/Abhishek2271/TransferabilityAnalysis/wiki) section.
+
+# References
+## Quantization is based on DoReFa-Net method as proposed in the paper: https://arxiv.org/abs/1606.06160  
+Cited as:  
+Zhou, S., Wu, Y., Ni, Z., Zhou, X., Wen, H., & Zou, Y. (2018). DoReFa-net: Training low bitwidth convolutional neural networks with low bitwidth gradients. arXiv:1606.06160 [cs].
+
+#### For quantization, the library available from the authors is used. This is available at:
+https://github.com/tensorpack/tensorpack/tree/master/examples/DoReFa-Net
+
+The networks used are from the examples provided on the Tensorpack repository:   
+Tensorpack cited as:  
+Wu, Y. et al. (2016). Tensorpack. https://github.com/tensorpack.
+
+#### Tensorpack models are available at:  
+https://github.com/tensorpack/tensorpack/tree/master/examples
+
+## Adversarial Examples are created using Adversarial Robustness Toolbox (ART) v. 1.5.1. Official paper: https://arxiv.org/abs/1807.01069  
+Cited as:  
+Nicolae, M.-I., Sinn, M., Tran, M. N., Buesser, B., Rawat, A., Wistuba, M., Zantedeschi, V., Baracaldo, N., Chen, B., Ludwig, H., Molloy, I. M., & Edwards, B. (2019). Adversarial robustness toolbox v1.0.0. arXiv:1807.01069
+
+ART is one of the popular APIs for adversaral examples generation and supports a large number of attacks. It is open-source with large number of very well explained examples. Please check out their repository at:
+https://github.com/Trusted-AI/adversarial-robustness-toolbox
---
+++ visualize_data.py
@@ -0,0 +1,143 @@
+import matplotlib.pyplot as plt
+from matplotlib import image
+import pandas as pd
+from matplotlib import colors
+from IPython.display import display
+
+# Pandas used for plotting transferability matrix. 
+# Pandas: © Copyright 2008-2022, the pandas development team.
+# For more information pls visit:https://pandas.pydata.org/pandas-docs/stable/user_guide/style.html
+# MathPlot lib used for plotting images.
+# MathPlot: © Copyright 2002 - 2012 John Hunter, Darren Dale, Eric Firing, Michael Droettboom and the Matplotlib development team; 2012 - 2021 The Matplotlib development team.
+# For more insformation pls visit https://matplotlib.org/stable/api/index
+
+'''
+Use matplotlib imshow to plot a single image.
+
+args:
+data: a single image numpy array (for instance, [28,28] for a MNIST image)
+for more details on image plots please follow the matplot lib guide: https://matplotlib.org/3.5.1/tutorials/introductory/images.html
+'''
+def plot_image(data):
+    #fig = plt.figure(figsize=(15, 7))
+    plt.title("Adversial examples")
+    plt.imshow(data,cmap=plt.cm.gray, interpolation='nearest')
+    plt.show() 
+
+    
+'''
+Use matplotlib imshow to plot multiple images.
+
+args:
+data: image numpy array of multiple image (for instance,[100,28,28] for an MNIST image)
+'''    
+def plot_images(data):
+    # plot the images: each image is 28x28 pixels
+    # set up the figure
+    fig = plt.figure(figsize=(15, 7))
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+    #for now, only show top 50
+    if(data.shape[0]<=50):
+        total_number = data.shape[0]
+    else:
+        total_number = 50
+    for i in range(total_number):
+        #ax = fig.add_subplot(10, 10, i + 1, xticks=[], yticks=[])
+        ax = fig.add_subplot(5, 10, i + 1, xticks=[], yticks=[])
+        #here the interpolation controls "anti-aliasing". For our case this should be strictly "none" or "nearest" because
+        #we dont need antiliasing since most of our decisions on selecting hyperparameters are based on visual clues
+        #ax.imshow(data[i].reshape((32,32,3)), interpolation='nearest')
+        ax.imshow(data[i].reshape((28,28)),cmap=plt.cm.gray, interpolation='nearest')
+        #ax.text(0, 7, i, color='red', weight="bold")
+        #ax.imshow(data[i].reshape((28,28)),cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.show()
+
+'''
+Plot the pandas dataframe with gradients. The dataframe here is the transferability data obtained from the experiments.
+For more details on background gradients please refer to pandas manual at: https://pandas.pydata.org/docs/reference/api/pandas.io.formats.style.Styler.background_gradient.html
+
+This example is referred from stackoverflow: https://stackoverflow.com/questions/38931566/pandas-style-background-gradient-both-rows-and-columns
+The function overrides pandas "background" function: https://github.com/pandas-dev/pandas/blob/29d81f3df81eb0a4d077ae1317df74d509cdc446/pandas/formats/style.py#L817
+'''
+def plot_data_points(data_frame):
+    df = pd.DataFrame(data_frame)
+    pd.set_option("display.precision", 3)
+    df.columns= ["Float", "1" ,"2", "4", "8", "12", "16"]
+    df.index = ["Float", "1", "2", "4", "8", "12", "16"]
+    #visualize_data.plotTable(df)
+    # generate some example data
+    def background_gradient(s, m, M, cmap='PuBu', low=0, high=0):
+        rng = M - m
+        norm = colors.Normalize(m - (rng * low),
+                                M + (rng * high))
+        normed = norm(s.values)
+        c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
+        return ['background-color: %s' % color for color in c]
+
+    display(df.style.apply(background_gradient,
+                cmap='PuBu',
+                m=df.min().min(),
+                M=df.max().max(),
+                low=0,
+                high=0.2))
+
+'''    
+Display transferability matrix with average of the entire row at the end.
+This is a slight modification of the function "plot_data_points(dataframe)" of this class.
+'''   
+def plot_data_points_with_average(data_frame):
+    df = pd.DataFrame(data_frame)
+    pd.set_option("display.precision", 3)
+    df.columns= ["Float", "1" ,"2", "4", "8", "12", "16", "Average"]
+    df.index = ["Float", "1", "2", "4", "8", "12", "16"]
+    #visualize_data.plotTable(df)
+
+    def background_gradient(s, m, M, cmap='PuBu', low=0, high=0):
+        rng = M - m
+        norm = colors.Normalize(m - (rng * low),
+                                M + (rng * high))
+        normed = norm(s.values)
+        c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
+        return ['background-color: %s' % color for color in c]
+
+    display(df.style.apply(background_gradient,
+                cmap='PuBu',
+                m=df.min().min(),
+                M=df.max().max(),
+                low=0,
+                high=0.2))
+
+'''
+Same as plot_data_points_with_average but to make the labels look good, rows and column label backgrounds are made a bit consistent in color.
+For more details and examples on this, please follow: https://pandas.pydata.org/docs/user_guide/style.html
+'''
+def plot_data_points_with_average_stylized(data_frame):
+    df = pd.DataFrame(data_frame)
+    pd.set_option("display.precision", 3)
+    df.columns= ["FP", "1" ,"2", "4", "8", "12", "16", "Average"]
+    df.index = ["FP", "1", "2", "4", "8", "12", "16"]
+    
+    def background_gradient(s, m, M, cmap='PuBu', low=0, high=0):
+        rng = M - m
+        norm = colors.Normalize(m - (rng * low),
+                                M + (rng * high))
+        normed = norm(s.values)
+        c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
+        return ['background-color: %s' % color for color in c]
+
+    th_props = [
+                ('font-weight', 'bold'),
+                ('background-color', '#f7f7f9')
+                ]
+
+    # Set table styles
+    styles = [
+        dict(selector="th", props=th_props)
+        ]
+
+    display(df.style.apply(background_gradient,
+                cmap='PuBu',
+                m=df.min().min(),
+                M=df.max().max(),
+                low=0,
+                high=0.2).set_table_styles(styles))
